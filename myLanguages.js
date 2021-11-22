d2eaf7c6c5f7f71e941980e554711f589be86d24
Andrew <andreialekseevich51@gmail.com>
2021-11-22 18:18:01 +0300

    myLang task

--- myLanguages.js
+++ myLanguages.js
@@ -1,9 +1,24 @@
 function myLanguages(results) {
-    const res = Object.entries(results).filter(([key, value]) => value >=60)
-    
-    console.log(res[0], res[1])
+  const res = Object.entries(results)
+    .filter(([key, value]) => value >= 60)
+    .sort((a, b) => {
+      if (a[1] > b[1]) {
+        return -1;
+      }
+      return 1;
+    })
+    .map((item) => {
+      return item[0];
+    });
 
-    return res 
+  return res;
 }
 
-console.log(myLanguages({"Java": 10, "Ruby": 80, "Python": 65}))
+const results = {
+  Java: 10,
+  Ruby: 80,
+  Python: 65,
+};
+
+console.log(myLanguages(results));
+// const res = Object.entries(dict).filter(([key, value]) => value ===  "squeak").map(([key]) => key)
